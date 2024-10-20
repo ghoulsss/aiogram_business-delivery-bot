e0cc7b17d3d71613bedc2252aeff24a841e2736d
Pavlov Danil <danpavkzn@mail.ru>
2024-10-20 06:23:40 +0300

    change listname

--- sheets.py
+++ sheets.py
@@ -16,7 +16,7 @@ service = build("sheets", "v4", credentials=credentials)
 data_sheet = (
     service.spreadsheets()
     .values()
-    .get(spreadsheetId=spreadsheet_id, range="Лист1")
+    .get(spreadsheetId=spreadsheet_id, range="пользователи")
     .execute()
     .get("values", [])
 )
