da5ae84cdafa42cd6249953d5ef19629417de8bd
Pavlov Danil <danpavkzn@mail.ru>
2024-10-21 04:12:06 +0300

    litl bit

--- handlers.py
+++ handlers.py
@@ -3,17 +3,16 @@ from aiogram import types
 from aiogram.filters import Command
 from sheets import super_user, admin_sklada, courier
 
-allowed_users=[super_user, admin_sklada, courier]
+allowed_users = [super_user, admin_sklada, courier]
 
 router = Router()
 
-
-@router.message(Command(commands=['start']))
+@router.message(Command(commands=["start"]))
 async def test_start(message: types.Message):
     try:
         if message.from_user.id in allowed_users:
-            await message.reply(f'Привет я работаю!,у тебя есть доступ {message.from_user.id}')
+            await message.answer(f'Привет я работаю!,у тебя есть доступ')
         else:
-             await message.reply(f'У тебя нет доступа {message.from_user.id}')
+            await message.answer(f'У тебя нет доступа')
     except Exception as e:
         print('что-то пошло не так я не работаю, причина:', e)
