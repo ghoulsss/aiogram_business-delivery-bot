7e8008fd26e41e44726ba8a19c9d6d148856580b
Pavlov Danil <danpavkzn@mail.ru>
2024-10-30 12:52:50 +0300

    правки

--- handlers/callback.py
+++ handlers/callback.py
@@ -294,8 +294,8 @@ async def reglament_callback(callback: CallbackQuery):
 async def reglament_callback(callback: CallbackQuery):
     worksheet_sort = sh.worksheet("Сортировка")
     worksheet_zada = sh.worksheet("Задание")
-    worksheet_sort.batch_clear(["A8:D"])  # УКАЗАТЬ ВСЮ ТАБЛИЦУ ["A2:D"]
-    worksheet_zada.batch_clear(["A8:G"])  # УКАЗАТЬ ВСЮ ТАБЛИЦУ ["A2:G"]
+    worksheet_sort.batch_clear(["A2:D"])  # УКАЗАТЬ ВСЮ ТАБЛИЦУ ["A2:D"]
+    worksheet_zada.batch_clear(["A2:G"])  # УКАЗАТЬ ВСЮ ТАБЛИЦУ ["A2:G"]
     await callback.message.edit_text(text="Сортировка и Задание очищены")
     await callback.answer("")
 
